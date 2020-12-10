3a2d13d09ac3a07ca13839aa3e164d9cd6af9de4
DAM2_SUB1 <DAM2_SUB1@PC16>
2020-12-10 15:59:56 +0100

    iniciamos validacion

--- Practica_Primera_Evaluacion_Carreras_Deportivas/src/Logica/Validar.java
+++ Practica_Primera_Evaluacion_Carreras_Deportivas/src/Logica/Validar.java
@@ -20,12 +20,8 @@ public class Validar {
     /* Este metodo valida el DNI del particiante */
     
     public boolean V_DNI (String DNI){
-        char[] letrasDni = DNI.toCharArray();
-        if(!DNI.equals("")){
-            return true;
-        } else{
-            return false;
-        }
+        //valida que tenga 8 numeros del 0-9 y la letra del dni
+        return DNI.matches("^[0-9]{8}[T|R|W|A|G|M|Y|F|P|D|X|B|N|J|Z|S|Q|V|H|L|C|K|E]$");
     }
     
     /* Este metodo valida el Fecha_Nacimiento del particiante */
@@ -56,7 +52,7 @@ public class Validar {
     public boolean V_N_Contacto(int N_Contacto){
         int n1 = 100000000;
         int n2 = 999999999;
-        if(N_Contacto < n1 || N_Contacto > n2 ){
+        if(N_Contacto > n1 || N_Contacto < n2 ){
             return true;
         } else{
             return false;
